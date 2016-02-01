0f938ae8ff1d9026b45e64c3ca7a33170d9efd4c
Brendan Coates <bcoates@ucsb.edu>
2016-02-01 14:15:49 -0800

    avlab-cyls updated to include hashmove

--- avlab-cylinders.cmd
+++ avlab-cylinders.cmd
@@ -21,10 +21,12 @@ for /f "tokens=*" %%g in ('dir /b /s /a:d /o:n "R:\Cylinders\avlab\audio_capture
 	set processingDir=R:\Cylinders\avlab\audio_captures\!cylNum!\processingDir\
 	set scratchDir=T:\avlab\projects\cylinder-scratch\!cylNum!\
 	CALL :pre-process-verify
+	echo "pre process done"
 	CALL :make-broadcast-master
+	echo "make broadcast master done"
 	CALL :make-mp3
+	echo "makemp3 done"
 	CALL :move-to-repo
-	GOTO :eof
 )
 
 
@@ -49,24 +51,12 @@ if not exist !broadcastObj! (
 	ping -n 10 127.0.0.1>nul
 	rd !startDir!
 )
-if not exist !mtdObj! (
-	md !scratchDir!
-	xcopy !startDir! !scratchDir! /y /i
-	del *.* /q
-	ping -n 10 127.0.0.1>nul
-	rd !startDir!
-)
 popd
-	
-
-ping -n 5 127.0.0.1>nul
 GOTO :eof
 
 :make-broadcast-master
-
 pushd !startDir!
 md !processingDir!
-	
 ::run this extension on the broadcast master and find where to put the fades
 ffprobe -i !broadcastObj! -show_entries format=duration -v quiet -of csv="p=0" > R:\Cylinders\avlab\_tmpLength.txt
 ::loop thru the contents of that file (1 text string)
@@ -83,19 +73,21 @@ for /f "delims=" %%i in (R:\Cylinders\avlab\_tmpLength.txt) do (
 	::recombine the whole-number fadeout start time with the decimal value from earlier and we get a 2s fadeout that ends exactly when the wav ends
 	set fadeoutStart=!_fadeoutStart!.!decimal!
 	::call ffmpeg with the input, set the audio channel to downmix to 1 (mono), set sample rate to 44100, set the fades, insert calculated start time form earlier, set the sample rate, set the output
-	ffmpeg -i !broadcastObj! -ac 1 -ar 44100 -af afade=t=in:ss=0:d=2,afade=t=out:st=!fadeoutStart!:d=2 -sample_fmt s16 !processingDir!!broadcastObj!
+	ffmpeg -i !broadcastObj! -ac 1 -ar 44100 -af afade=t=in:ss=0:d=2,afade=t=out:st=!fadeoutStart!:d=2 -sample_fmt s16 -id3v2_version 3 -write_id3v1 1 !processingDir!!broadcastObj!
 	::move the processed broadcast master and overwrite the previous broadcast master
 	move /y !processingDir!!broadcastObj! !startDir!\!broadcastObj!
 )
 ::delete the txt file with the length string in it
 del R:\Cylinders\avlab\_tmpLength.txt
+rd !processingDir!
+popd
 GOTO :eof
 
 
 
 :make-mp3
 pushd !startDir!
-S:\avlab\bash-microservices\makemp3.sh !broadcastObj! R:\Cylinders\avlab\audio_captures\!cylNum!\
+ffmpeg -i !broadcastObj! -ar 44100 -ab 320k -f mp3 -id3v2_version 3 -write_id3v1 1 !endObj!
 popd 
 GOTO :eof
 
@@ -104,7 +96,7 @@ GOTO :eof
 ::test to find out the directory structure for saved files
 if "!cylNum:~0,1!"=="0" (
 	set endDir=R:\Cylinders\0000\!cylNum!
-	xcopy !startDir! !endDir! /y /i
+	hashmove -folder !startDir! !endDir!
 )
 ::if first digit more than 0 it enters this condition, and we ask if it's more or less than 10000
 if not "!cylNum:~0,1!"=="0" (
@@ -114,53 +106,17 @@ if not "!cylNum:~0,1!"=="0" (
 	if !bar! GTR !fubar! (
 		set cylNum_fiveDigit=!cylNum:~0,2!
 		set destThousandDir=!cylNum_fiveDigit!000
-		set endDir=R:\Cylinders\!destThousandDir!\!cylNum!\
-		xcopy !startDir! !endDir! /y /i
+		set endDir=R:\Cylinders\!destThousandDir!\!cylNum!
+		hashmove -folder !startDir! !endDir!
 	)
 	if !bar! LSS !fubar! (
 		set cylNum_fourDigit=!cylNum:~0,1!
 		set destThousandDir=!cylNum_fourDigit!000
-		set endDir=R:\Cylinders\!destThousandDir!\!cylNum!\
-		xcopy !startDir! !endDir! /y /i
+		set endDir=R:\Cylinders\!destThousandDir!\!cylNum!
+		hashmove -folder !startDir! !endDir!
 	)
 )
 
-::generate checksums for orig and migrated directories
-fsum !startDir! /t:e 
-fsum !endDir! /t:e 
-pushd !startDir!
-::go back into the start directory
-for /r %%j in (*.md5) do (
-	set startObj=%%j
-	::set the drive
-	set "d=%%~dj"
-	::set the path
-	set "p=%%~pj"
-	::set a name for this object
-	set "n=%%~nj"
-	set x=%%~xj
-	::set a variable to the .md5 file on the R:\ drive
-	set endObj=!endDir!!n!!x!
-	::grab the string from the .md5 file in the capture directory
-	for /f "tokens=*" %%k in (!startObj!) do (
-		set preCopy=%%k
-		::grab the string from the .md5 file in the repository directory
-		for /f "tokens=*" %%m in (!endObj!) do (
-			set postCopy=%%m
-			::ask if they are the same string
-			if !preCopy!==!postCopy! (
-				::if they are, delete the .wav and then delete the .md5
-				::this notation works because the MD5 file is "named" name.wav
-				::so the .md5 is the extension, and the .wav is part of the "name"
-				::but that "name" also corresponds to a file that we can work with
-				::i just think that is so cool
-				del !d!!p!!n!
-				del !d!!p!!n!!x!
-			)
-		)
-	)
-)
-popd
 GOTO :eof
 
 :End
--- hashmove.cmd
+++ hashmove.cmd
@@ -7,10 +7,11 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-
+::set arguments to vars
 set flag=%1
 set startDir=%2
 set endDir=%3
+
 ::check that the inputs are formatted right
 set _sd-backslash=!startDir:~-1!
 set _ed-backslash=!endDir:~-1!
@@ -18,31 +19,32 @@ if not !_sd-backslash!==\ (
 	set startDir=!startDir!\
 )
 if not !_ed-backslash!==\ (
-	set startDir=!endDir!\
+	set endDir=!endDir!\
 )
 
 ::hashmove for folders
 if !flag!==-folder (
-
-pushd !startDir!
-for /r %%j in (*) do (
-	if not %%~xj==.md5 (
-		set flag=%%~nj%%~xj
-		CALL :hashmovefiles !flag!
-		GOTO :eof
+	pushd !startDir!
+	for /r %%j in (*) do (
+		if not %%~xj==.md5 (
+			set flag=%%~nj%%~xj
+			CALL :hashmovefiles !flag!
+		)
 	)
-)
-popd
+	popd
+	ping -n 5 127.0.0.1>nul
+	rd !startDir!
 )
 
-
+::hashmove for files
+::var names get confusing here
 if not !flag!==-folder (
 	:hashmovefiles
 	robocopy !startDir! !endDir! !flag!
 	if not exist !startDir!!flag!.md5 (
-			fsum !startDir!!flag! !startDir!!flag!.md5
+			fsum !startDir!!flag! !startDir!!flag!.md5 /o
 	)
-	fsum !endDir!!flag! !endDir!!flag!.md5
+	fsum !endDir!!flag! !endDir!!flag!.md5 /o
 	pushd !startDir!
 	::loop through the directory we've been working with, and for every .md5 file, compare the string to the .md5 file in the destination
 	for /r %%j in (*.md5) do (
