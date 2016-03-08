c77a1fe6a5e1709d8e8a5f6a21db90808ea8a890
Brendan Coates <bcoates@ucsb.edu>
2016-03-08 14:34:09 -0800

    hashmove.py dest backslash fixed

--- hashmove.py
+++ hashmove.py
@@ -1,24 +1,26 @@
 import hashlib
 import os
 import sys
-import ntpath
 import shutil
-import difflib
 
 
 startObj = sys.argv[1] # grab argument from CL
 startObj = startObj.replace("\\","/") # fun fact, windows lets you type both fwd and bck slashes in pathnames
 dest = sys.argv[2]
 dest = dest.replace("\\","/") # gotta have trailing slash here
-endObj = dest + os.path.basename(startObj)
+if dest.endswith != "/": # check for said trailing slash
+	dest = dest + "/"
+endObj = dest + os.path.basename(startObj) # this is the file that we wanted to move, in its destination
 soMD = startObj + ".md5"
 eoMD = endObj + ".md5"
-shutil.copy(startObj, dest)
-# soPath, soFname = os.path.split(startObj) placeholder
 flist = [startObj, endObj]
 blocksize = 65536 # reading a file in chunks this size more memory efficient
 hasher = hashlib.md5() # specify method/ hashing algorithm
+# soPath, soFname = os.path.split(startObj) placeholder
 
+shutil.copy(startObj, dest) # actually copy the file from start to finish
+
+#here's where the fun starts, generate checksums for both source and dest
 for f in flist:
 	fmd5 = f + ".md5" # concat with ext to make md5 file names
 	with open(f, 'rb') as afile: # open da file
@@ -30,19 +32,22 @@ for f in flist:
 		text_file.write(hasher.hexdigest() + " *" + startObj) # write it with the hash *filename
 	print(hasher.hexdigest()) # print it to the screen too who cares
 
-
+# open the hash files and compare their contents
 with open(soMD,'r') as f1:
 	with open(eoMD,'r') as f2:
 		sf1 = f1.read()
 		ef2 = f2.read()
 		if sf1 == ef2:
-			delyn = 1
+			delyn = 1 # set an error level
 		else:
 			delyn = 0
+			
+# if the strings match it's ok to delete these start file and its hash	
 if delyn == 1:
 	f1.close()
 	os.remove(startObj)
 	os.remove(soMD)
 else:
 	print "uh there was an issue there"
-			
+
+	
