719549f5eda59e4abcc746c867532cd0d366a235
Brendan Coates <bcoates@ucsb.edu>
2016-07-08 11:06:48 -0700

    qc script for listening to transfers n days old

--- avlab-audio.py
+++ avlab-audio.py
@@ -32,7 +32,7 @@ class cd:
 def deletebs(captureDir):
 	for dirs,subdirs,files in os.walk(captureDir):
 		for f in files:
-			if f.endswith(".gpk") of f.endswith(".mrk") or f.endswith(".bak"):
+			if f.endswith(".gpk") or f.endswith(".mrk") or f.endswith(".bak"):
 				os.remove(os.path.join(captureDir,f))
 	return
 		
@@ -50,117 +50,109 @@ def makelist(captureDir,toProcessDir,flist = {}):
 	return flist
 
 #do the ffmpeg stuff to each file	
-def ffprocess(flist,captureDir,mmrepo):
-	for f in sorted(flist):
-		opts = flist[f]
-		aNumber = "a" + str(opts[0])
-		dirNumber = aNumber
-		if aNumber.endswith("A") or aNumber.endswith("B"):
-			dirNumber = aNumber[:-1]
-		try:
-			channelConfig = str(opts[1])
-		except IndexError:
-			channelConfig = "nothing"
-		processingDir = os.path.join(captureDir,dirNumber)
-		endObj1 = os.path.join(processingDir,"cusb-" + aNumber + "a.wav") #name of archival master when we're done
-		endObj2 = os.path.join(processingDir,"cusb-" + aNumber + "e.wav")
-		#make a processing directory named after first attr in fm export: aNumber
-		if not os.path.exists(processingDir):
-			os.makedirs(processingDir)
-			#remove silence from raw transfer if audio quieter than -50dB, longer than 10s of silence
-		if not os.path.exists(endObj1):
-			subprocess.call('ffmpeg -i ' + os.path.join(captureDir,f) + '.wav -af silenceremove=0:0:-50dB:-10:1:-50dB -acodec pcm_s24le ' + endObj1) 
-		#let's make sure the channels are right
-		with cd(processingDir):
-			#the following call pipes the ffprobe stream output back to this script
-			ffdata = subprocess.Popen(["ffprobe","-show_streams","-of","flat",endObj1],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-			data, err = ffdata.communicate() #separate it so it's useful
-			print channelConfig
-			if "stereo" in data: #ok so all of our raw captures are stereo so this ~should~ always trigger
-				if channelConfig == '1/4-inch Half Track Mono':
-					subprocess.call(["python",os.path.join(mmrepo,"changechannels.py"),"-so",endObj1]) #call change channels to split streams to separate files, renaming them correctly
-				if channelConfig == '1/4-inch Full Track Mono':
-					subprocess.call(["ffmpeg","-i",endObj1,"-ac","1",endObj2]) #downmix to mono
-					os.remove(endObj1) #can't overwrite with ffmpeg it's trash
-					os.rename(endObj2,endObj1)
-				
-			#split files larger than 2GB	
-			#statinfo = os.stat(endObj1)
-			#eo1size = statinfo.st_size
-			#if eo1size >= 2147483648:
-				#print "too big too wide"
+def ffprocess(f,opts,captureDir,mmrepo):
+	aNumber = "a" + str(opts[0])
+	dirNumber = aNumber
+	if aNumber.endswith("A") or aNumber.endswith("B"):
+		dirNumber = aNumber[:-1]
+	try:
+		channelConfig = str(opts[1])
+	except IndexError:
+		channelConfig = "nothing"
+	processingDir = os.path.join(captureDir,dirNumber)
+	endObj1 = os.path.join(processingDir,"cusb-" + aNumber + "a.wav") #name of archival master when we're done
+	endObj2 = os.path.join(processingDir,"cusb-" + aNumber + "e.wav")
+	#make a processing directory named after first attr in fm export: aNumber
+	if not os.path.exists(processingDir):
+		os.makedirs(processingDir)
+		#remove silence from raw transfer if audio quieter than -50dB, longer than 10s of silence
+	if not os.path.exists(endObj1):
+		subprocess.call('ffmpeg -i ' + os.path.join(captureDir,f) + '.wav -af silenceremove=0:0:-50dB:-10:1:-50dB -acodec pcm_s24le ' + endObj1) 
+	#let's make sure the channels are right
+	with cd(processingDir):
+		#the following call pipes the ffprobe stream output back to this script
+		ffdata = subprocess.Popen(["ffprobe","-show_streams","-of","flat",endObj1],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+		data, err = ffdata.communicate() #separate it so it's useful
+		print channelConfig
+		if "stereo" in data: #ok so all of our raw captures are stereo so this ~should~ always trigger
+			if channelConfig == '1/4-inch Half Track Mono':
+				subprocess.call(["python",os.path.join(mmrepo,"changechannels.py"),"-so",endObj1]) #call change channels to split streams to separate files, renaming them correctly
+			if channelConfig == '1/4-inch Full Track Mono':
+				subprocess.call(["ffmpeg","-i",endObj1,"-ac","1",endObj2]) #downmix to mono
+				os.remove(endObj1) #can't overwrite with ffmpeg it's trash
+				os.rename(endObj2,endObj1)
+			
+		#split files larger than 2GB	
+		#statinfo = os.stat(endObj1)
+		#eo1size = statinfo.st_size
+		#if eo1size >= 2147483648:
+			#print "too big too wide"
 	return
 
 #do the fancy library thing to each file	
-def bextprocess(flist,bextsDir,captureDir):
-	for f in flist:
-		opts = flist[f]
-		aNumber = "a" + str(opts[0])
-		
-		dirNumber = aNumber
-		if aNumber.endswith("A") or aNumber.endswith("B"):
-			dirNumber = aNumber[:-1]
-		processingDir = os.path.join(captureDir,dirNumber)
-		endObj1 = os.path.join(processingDir,"cusb-" + aNumber + "a.wav")
-		endObj1A = os.path.join(processingDir,"cusb-" + aNumber + "Aa.wav")
-		endObj1B = os.path.join(processingDir,"cusb-" + aNumber + "Ba.wav")
-		#clear mtd already in there
-		subprocess.call('bwfmetaedit --in-core-remove ' + endObj1)
-		#embed checksums
-		print "hashing data chunk of " + aNumber
-		subprocess.call('bwfmetaedit --MD5-Embed-Overwrite ' + endObj1)
-		#embed bext metadata based on FM output
-		bextFile = os.path.join(bextsDir,'cusb-' + aNumber + '-bext.txt')
-		if os.path.exists(bextFile):
-			print "embedding bext in " + aNumber
-			with open(bextFile) as bf:
-				bextlst = bf.readlines()
-				bextstr = str(bextlst)
-				bextstr = bextstr.strip("['']")
-				#bextstr = bextstr.replace('"','')
-				foo = 'bwfmetaedit ' + bextstr + ' ' + endObj1
-				if os.path.exists(endObj1):
-					subprocess.call('bwfmetaedit ' + bextstr + ' ' + endObj1)
-				elif os.path.exists(endObj1A):
-					subprocess.call('bwfmetaedit ' + bextstr + ' ' + endObj1A)
-				elif os.path.exists(endObj1B):
-					subprocess.call('bwfmetaedit ' + bextstr + ' ' + endObj1B)
-				#print foo
+def bextprocess(f,opts,bextsDir,captureDir):
+	aNumber = "a" + str(opts[0])
+	dirNumber = aNumber
+	if aNumber.endswith("A") or aNumber.endswith("B"):
+		dirNumber = aNumber[:-1]
+	processingDir = os.path.join(captureDir,dirNumber)
+	endObj1 = os.path.join(processingDir,"cusb-" + aNumber + "a.wav")
+	endObj1A = os.path.join(processingDir,"cusb-" + aNumber + "Aa.wav")
+	endObj1B = os.path.join(processingDir,"cusb-" + aNumber + "Ba.wav")
+	#clear mtd already in there
+	subprocess.call('bwfmetaedit --in-core-remove ' + endObj1)
+	#embed checksums
+	print "hashing data chunk of " + aNumber
+	subprocess.call('bwfmetaedit --MD5-Embed-Overwrite ' + endObj1)
+	#embed bext metadata based on FM output
+	bextFile = os.path.join(bextsDir,'cusb-' + aNumber + '-bext.txt')
+	if os.path.exists(bextFile):
+		print "embedding bext in " + aNumber
+		with open(bextFile) as bf:
+			bextlst = bf.readlines()
+			bextstr = str(bextlst)
+			bextstr = bextstr.strip("['']")
+			#bextstr = bextstr.replace('"','')
+			foo = 'bwfmetaedit ' + bextstr + ' ' + endObj1
+			if os.path.exists(endObj1):
+				subprocess.call('bwfmetaedit ' + bextstr + ' ' + endObj1)
+			elif os.path.exists(endObj1A):
+				subprocess.call('bwfmetaedit ' + bextstr + ' ' + endObj1A)
+			elif os.path.exists(endObj1B):
+				subprocess.call('bwfmetaedit ' + bextstr + ' ' + endObj1B)
+			#print foo
 	return
 
 #hashmove each file to the repo	
-def move(flist,captureDir,mmrepo,archRepoDir):
-	for f in flist:
-		opts = flist[f]
-		aNumber = "a" + str(opts[0])
-		dirNumber = aNumber
-		if aNumber.endswith("A") or aNumber.endswith("B"):
-			dirNumber = aNumber[:-1]
-		processingDir = os.path.join(captureDir,dirNumber)
-		if os.path.isdir(processingDir):
-			endDirThousand = aNumber.replace("a","") #input arg here is a1234 but we want just the number
-			#the following separates out the first digit and assigns an appropriate number of zeroes to match our dir structure
-			if len(endDirThousand) < 5:
-				endDirThousand = endDirThousand[:1] + "000"
-			else:
-				endDirThousand = endDirThousand[:2] + "000"
-			endDir = os.path.join(archRepoDir,endDirThousand,dirNumber)
-			#hashmove it and grip the output so we can delete the raw files YEAHHH BOY
-			output = subprocess.Popen(['python',os.path.join(mmrepo,'hashmove.py'),processingDir,endDir],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-			foo,err = output.communicate()
-			sourcehash = re.search('srce\s\S+\s\w{32}',foo)
-			desthash = re.search('dest\s\S+\s\w{32}',foo)
-			dh = desthash.group()
-			sh = sourcehash.group()
-			bexttxt = os.path.join("R:/audio/avlab/fm-exports/bexttxts","cusb-" + dirNumber + "-bext.txt")
-			startObj1 = os.path.join("R:/audio/avlab/fm-exports/to_process",f + ".txt")
-			if sh[-32:] == dh[-32:]:
-				os.remove(os.path.join(captureDir,f + ".wav"))
-				if os.path.exists(bexttxt): #can't give os.remove a file object it's gotta be a string grrrrr
-					os.remove(os.path.join("R:/audio/avlab/fm-exports/bexttxts","cusb-" + dirNumber + "-bext.txt"))
-				if os.path.exists(startObj1):
-					os.remove(os.path.join("R:/audio/avlab/fm-exports/to_process",f + ".txt"))
-	
+def move(f,opts,captureDir,mmrepo,archRepoDir):
+	aNumber = "a" + str(opts[0])
+	dirNumber = aNumber
+	if aNumber.endswith("A") or aNumber.endswith("B"):
+		dirNumber = aNumber[:-1]
+	processingDir = os.path.join(captureDir,dirNumber)
+	if os.path.isdir(processingDir):
+		endDirThousand = aNumber.replace("a","") #input arg here is a1234 but we want just the number
+		#the following separates out the first digit and assigns an appropriate number of zeroes to match our dir structure
+		if len(endDirThousand) < 5:
+			endDirThousand = endDirThousand[:1] + "000"
+		else:
+			endDirThousand = endDirThousand[:2] + "000"
+		endDir = os.path.join(archRepoDir,endDirThousand,dirNumber)
+		#hashmove it and grip the output so we can delete the raw files YEAHHH BOY
+		output = subprocess.Popen(['python',os.path.join(mmrepo,'hashmove.py'),processingDir,endDir],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+		foo,err = output.communicate()
+		sourcehash = re.search('srce\s\S+\s\w{32}',foo)
+		desthash = re.search('dest\s\S+\s\w{32}',foo)
+		dh = desthash.group()
+		sh = sourcehash.group()
+		bexttxt = os.path.join("R:/audio/avlab/fm-exports/bexttxts","cusb-" + dirNumber + "-bext.txt")
+		startObj1 = os.path.join("R:/audio/avlab/fm-exports/to_process",f + ".txt")
+		if sh[-32:] == dh[-32:]:
+			os.remove(os.path.join(captureDir,f + ".wav"))
+			if os.path.exists(bexttxt): #can't give os.remove a file object it's gotta be a string grrrrr
+				os.remove(os.path.join("R:/audio/avlab/fm-exports/bexttxts","cusb-" + dirNumber + "-bext.txt"))
+			if os.path.exists(startObj1):
+				os.remove(os.path.join("R:/audio/avlab/fm-exports/to_process",f + ".txt"))
 	return
 
 def main():
@@ -175,19 +167,21 @@ def main():
 	mmrepo = config.get('global','scriptRepo')
 
 	#get rid of the crap
-	deletebs(captureDir)
+	#deletebs(captureDir)
 	
 	#make a list of files to work on
 	flist = makelist(captureDir,toProcessDir)
-
-	#run the ffmpeg stuff we gotta do (silence removal, to add: changechannels and splitfiles)
-	ffprocess(flist,captureDir,mmrepo)
 	
-	#pop the bext info into each file
-	#bextprocess(flist,bextsDir,captureDir)
+	for f in flist:
+		opts = flist[f]
+		#run the ffmpeg stuff we gotta do (silence removal, to add: changechannels and splitfiles)
+		ffprocess(f,opts,captureDir,mmrepo)
+	
+		#pop the bext info into each file
+		bextprocess(f,opts,bextsDir,captureDir)
 
-	#hashmove them to the repo dir
-	move(flist,captureDir,mmrepo,archRepoDir)
+		#hashmove them to the repo dir
+		move(f,opts,captureDir,mmrepo,archRepoDir)
 	
 	return
 
---
+++ listen2transfers-qc.py
@@ -0,0 +1,80 @@
+#transfers-qc
+#a script that finds the most recent transfers and plays a few seconds from a sample of them
+
+import os
+import time
+import subprocess
+import re
+import ConfigParser
+import getpass
+import argparse
+from distutils import spawn
+
+#Context manager for changing the current working directory
+class cd:
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
+
+#check that we have the required software to run this script
+def dependencies():
+	depends = ['ffplay']
+	for d in depends:
+		if spawn.find_executable(d) is None:
+			print "Buddy, you gotta install " + d
+			sys.exit()
+	return
+
+def doit(sd,days):
+	qclist = []
+	youngerThanDays = days * 86400 #multiply it by seconds because that's what getmtime understands
+	present = time.time() #now
+	for dirs, subdirs, files in os.walk(sd):
+			for f in files:
+				fpath = os.path.join(dirs,f)
+				if (present - os.path.getmtime(fpath)) < youngerThanDays: #if the number of seconds that file has existed is less than the number of days we're listneing back
+					if fpath.endswith(".wav"): # and if the file is a wav
+						qclist.append(fpath) #append it to the lsit of files to search thru
+	foo = raw_input("Press any key to start listening")
+	for qcf in qclist[::3]: #listen to every third file in the list
+		print qcf
+		subprocess.call("ffplay -i " + qcf + " -ss 00:01:00.0") #play the file starting 1minute in
+	return
+	
+def main():
+	#initialize a buncha crap
+	parser = argparse.ArgumentParser(description="Listen to statistically significant random sampling of recent transfers")
+	parser.add_argument('-t','--tapes',action='store_true',default=False,help='adds 2s heads and tails fades to black/ silence')
+	parser.add_argument('-c','--cylinders',action='store_true',default=False,help='outputs to stereo (mono is default)')
+	parser.add_argument('-nj','--nationaljukebox',action='store_true',default=False,help='extra processing step for National Jukebox files')
+	parser.add_argument('-d','--days',default=7,type=int,help='the length of time to lsiten back to, in days')
+	args = vars(parser.parse_args()) #create a dictionary instead of leaving args in NAMESPACE land
+	searchDirs = [] #these are the dirs we'll search for new items
+	audioDirs = [] #"audio" or magnetic tapes have a special thing we gotta do
+	if args['tapes'] is True:
+		for s in os.listdir("R:/audio/"):
+			if s.endswith("0"): #soooo many dirs in this parent dir but everything we want to search will end with a 0
+				audioDirs.append(os.path.join("R:/audio",s)) #make a list of dirs, 0000 - 27000
+		audioDirs.reverse() #search thru later dirs first because they're more likely to have new stuff
+		for ad in audioDirs: 
+			searchDirs.append(ad) #pop each dir 1 at a time into our searchDirs list
+	if args['cylinders'] is True:
+		searchDirs.append("R:/Cylinders/avlab/audio_captures/") #search the capture directory (things that havent been processed)
+		searchDirs.append("R:/Cylinders") #search the repo (things that have been processed)
+	if args['nationaljukebox'] is True:
+		searchDirs.append("R:\78rpm\avlab\national_jukebox\in_process\pre-ingest-QC") #these files have been processed and they're waiting for images (generally)
+	
+	#print searchdirs
+	#foo = raw_input("eh")
+	for sd in searchDirs:
+		doit(sd,args['days'])
+	return
+
+dependencies()	
+main()
