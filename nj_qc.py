44f891ad115994214c338e75eec068aff057091b
Brendan Coates <bcoates@ucsb.edu>
2016-08-05 09:36:38 -0700

    nj_qc normie

--- nj_qc.py
+++ nj_qc.py
@@ -20,6 +20,22 @@ class cd:
     def __exit__(self, etype, value, traceback):
         os.chdir(self.savedPath)
 
+def makelist(qcDir, batchDir, extlist):
+	dirlist = []
+	#make a list of dirs that have everything we need
+	for dirs, subdirs, files in os.walk(qcDir): #loop thru qc dir
+		for s in subdirs: #loop thru each subdir
+			print "Verifying contents of " + s
+			if os.path.exists(os.path.join(batchDir,s)): #if it already exsists in our dest that's bad figure it out
+				print "Buddy, check on this SIP: " + s
+				print "It already exists in our batch directory"
+				break
+			#ok, dirs that make it here aren't in our batch dir already
+			with cd(os.path.join(dirs,s)): #cd into each subdir
+				if os.path.isfile(s + extlist[0]) and os.path.isfile(s + extlist[1]) and os.path.isfile(s + extlist[2]): #if each file extension exists in there
+					dirlist.append(s) #append the subdir to the list of files we wanna move
+	return dirlist
+		
 def main():
 	#initialize from the config file
 	config = ConfigParser.ConfigParser()
@@ -28,21 +44,15 @@ def main():
 	qcDir = config.get('NationalJukebox','PreIngestQCDir')
 	batchDir = config.get('NationalJukebox','BatchDir')
 	mmrepo = config.get('global','scriptRepo')
-	flist = ['m.wav','.wav','.tif']
-	dirlist = []
-	with cd(qcDir):
-		subprocess.call(['python',os.path.join(mmrepo,'rename_ucsbtocusb.py'),qcDir])
-	#make a list of dirs that have everything we need
-	for dirs, subdirs, files in os.walk(qcDir): #loop thru qc dir
-		for s in subdirs: #loop thru each subdir
-			print "Verifying contents of " + s
-			if os.path.exists(os.path.join(batchDir,s)): #if it already exsists in our dest that's real bad figure it out
-				print "Buddy, check on this SIP: " + s
-				print "It already exists in our batch directory"
-				break
-			with cd(os.path.join(dirs,s)): #cd into each subdir
-				if os.path.isfile(s + flist[0]) and os.path.isfile(s + flist[1]) and os.path.isfile(s + flist[2]): #if each file extension exists in there
-					dirlist.append(s) #append the subdir to the list of files we wanna move
+	extlist = ['m.wav','.wav','.tif']
+
+	#with cd(qcDir):
+	subprocess.call(['python',os.path.join(mmrepo,'rename_ucsbtocusb.py'),qcDir]) #rename them from ucsb to cusb
+	foor = raw_input("eh")
+	
+	dirlist = makelist(qcDir, batchDir, extlist)
+	
+	#now move them
 	for d in dirlist: #for each verified subdir
 		subprocess.call(['python',os.path.join(mmrepo,'hashmove.py'),os.path.join(qcDir,d),os.path.join(batchDir,d)]) #hashmove it to the batch folder
 				
