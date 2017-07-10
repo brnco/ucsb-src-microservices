74202a48876e4b3db254640b67914773165df5c3
Brendan Coates <bcoates@library.ucsb.edu>
2017-07-10 12:20:53 -0700

    logger.py updates

--- logger.py
+++ logger.py
@@ -4,6 +4,8 @@ import os
 import psutil
 import time
 import imp
+import getpass
+
 #makes a processID if none provided
 #searches up the stack for the top-most parent pid
 def makePID(pid):
@@ -11,7 +13,7 @@ def makePID(pid):
 		_pid = pid #set _pid to "old" pid
 		try:
 			pid = psutil.Process(pid).ppid() #try to set a new pid to the parent of the old pid
-			if psutil.Process(pid).name() == "cmd.exe": #if the parent of the old pid is cmd.exe
+			if psutil.Process(pid).name() == "cmd.exe" or psutil.Process(pid).name() == 'bash': #if the parent of the old pid is cmd.exe
 				return _pid #return the old pid
 		except:	#if the assignment didn't work
 			return _pid #return the old pid
@@ -29,6 +31,8 @@ def log(message,**kwargs):
 	rawconfig = imp.load_source('config',os.path.join(dn,'config.py'))
 	conf = rawconfig.config()
 	logLoc = conf.log.location
+	global ut
+	ut = imp.load_source("util",os.path.join(dn,"util.py"))
 	###END INIT###
 	###MAKE COMPONENTS###
 	if not 'pid' in kwargs:
@@ -45,7 +49,7 @@ def log(message,**kwargs):
 	else:
 		level = kwargs['level']
 	pidTime = str(psutil.Process(int(pid)).create_time())	
-	fname = os.path.join(logLoc,time.strftime("%Y-%m-%d"),pid + "-" + pidTime + ".log") #logs saved per day (prevent PID-named files from containing too many runs)
+	fname = os.path.join(logLoc,time.strftime("%Y-%m-%d"),getpass.getuser()+ "-" + pid + "-" + pidTime + ".log") #logs saved per day (prevent PID-named files from containing too many runs)
 	if not os.path.exists(os.path.dirname(fname)):
 		os.makedirs(os.path.dirname(fname))
 	###END MAKE###
