5de623e59f2f311d210ac0390386f426254f6314
Brendan Coates <bcoates@ucsb.edu>
2017-07-25 11:29:56 -0700

    catalog-stuff update for Alma

--- catalog-stuff.py
+++ catalog-stuff.py
@@ -9,7 +9,9 @@ import lxml
 
 def query(args):
 	#use aleph api to grip the catalog record for our object
-	url = 'http://pegasus.library.ucsb.edu:5661/sba01pub?version=1.1&operation=searchRetrieve&query=rec.id=' + args.sys + '&maximumRecords=1'
+	#obsolete aleph api call
+	#url = 'http://pegasus.library.ucsb.edu:5661/sba01pub?version=1.1&operation=searchRetrieve&query=rec.id=' + args.sys + '&maximumRecords=1'
+	url = "https://ucsb.alma.exlibrisgroup.com/view/sru/01UCSB_INST?version=1.2&operation=searchRetrieve&schema=marcxml&query=alma.mms_tagSuppressed%3Dfalse+and+alma.mms_id%3D" + args.sys + "%29"
 	response = urllib2.urlopen(url) #do that
 	xml = response.read() #gives back lovely marc xml
 	soup = BeautifulSoup(xml,'lxml') #soupify it for parsing
@@ -22,10 +24,16 @@ def main():
 	args = parser.parse_args()
 	id3list1 = []
 	id3list2 =[]
-	subtitle=''
-	title2=''
+	
 	list028ind0=[]
 	list028ind1=[]
+	subtitle=''
+	title1=''
+	title2=''
+	artist1=''
+	artist2=''
+	album=''
+	date=''
 	###END INIT###
 	
 	###GET XML SOUP FROM CATALOG
@@ -68,7 +76,7 @@ def main():
 		###GRIP ALBUM###
 		elif sf.parent['tag']==	'852': #grip the Columbia A69 part
 			if sf['code']=='j':
-				album = sf.string.lstrip()
+				album = sf.string.lstrip()		
 		###END ALBUM###
 		###GRIP LABEL###
 		elif sf.parent['tag']=='028':
@@ -87,18 +95,34 @@ def main():
 	###GET IT TOGETHER###
 	if subtitle:
 		title1 = title1 + subtitle
+	if not title1:
+		title1 = "Title Unavailable"
 	id3list1.append(title1)
+	if not artist1:
+		artist1 = "Artist Unavailable"
 	id3list1.append(artist1)
+	if not date:
+		date = "Date Unavailable"
 	id3list1.append(date)
+	if not album:
+		album = "Album Unavailable"
 	id3list1.append(album)
 	if list028ind1:
 		id3list1.append(list028ind1[0])
 	else:
 		id3list1.append(list028ind0[0])
 	if len(title2) > 0:
+		if not title2:
+			title2 = "Title Unavailable"
 		id3list2.append(title2)
+		if not artist2:
+			artist2 = "Artist Unavailable"
 		id3list2.append(artist2)
+		if not date:
+			date = "Date Unavailable"
 		id3list2.append(date)
+		if not album:
+			album = "Album Unavailable"
 		id3list2.append(album)
 		if list028ind1:
 			id3list2.append(list028ind1[1])
