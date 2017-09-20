8ff9626be18d92832be1a7a555eeab4f84a19fc7
specpa7 <specpa7@library.ucsb.edu>
2017-09-20 16:37:16 -0700

    updates

---
+++ utility-scripts/new-script-template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+import argparse
+import re
+import time
+import sys
+sys.path.insert(0,"S:/avlab/microservices")
+#remove ^ in production
+###UCSB modules###
+import config as rawconfig
+import util as ut
+import logger as log
+import mtd
+import makestartobject as makeso
+
+def main():
+	print "fubar"
+	
+if __name__ == '__main__':
+	main()
